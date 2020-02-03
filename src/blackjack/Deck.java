1e667a009cd31e577da624796cafe958955c1375
Kotagiri <kotagiri002@gannon.edu>
2020-02-03 16:48:44 -0500

    implemented shuffle method in deck

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -30,7 +30,12 @@ public class Deck {
         
     }
     private void shuffle(){
-        
+        for(int i = 0; i < myCards.length; i++){
+            Card temp = myCards[i];
+            int rand = (int)Math.random()*52;
+            myCards[i] = myCards[rand];
+            myCards[rand] = temp;
+        }
     }
     //printDeck is for testing only --delete me
     public void printDeck(){
